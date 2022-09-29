6c89fa506ecee0fec28b99a1a1e17f85890e6669
DavidHe <s991179@gmail.com>
2022-09-30 01:02:52 +0800

    init

---
+++ LfuCache_Test1/CacheItem.cs
@@ -0,0 +1,8 @@
+namespace LfuCache_Test1;
+
+public class CacheItem
+{
+    public string CacheKey { get; set; }
+    public string CacheValue { get; set; }
+    public int Frequency { get; set; }
+}
