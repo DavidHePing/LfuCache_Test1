b2c3730c95efbeec507293388161bc7421eff2da
DavidHe <s991179@gmail.com>
2022-09-30 02:25:22 +0800

    finish put

---
+++ LfuCache_Test1/LfuCache.cs
@@ -0,0 +1,72 @@
+namespace LfuCache_Test1;
+
+public class LfuCache
+{
+    private int _minFrequency = int.MaxValue; 
+    private readonly int _length;
+    private readonly Dictionary<int, LinkedList<CacheItem>> _frequencyList = new();
+    private readonly Dictionary<string, LinkedListNode<CacheItem>> _cache = new();
+
+    public LfuCache(int length)
+    {
+        _length = length;
+    }
+
+    public string? Get(string key)
+    {
+        if (!_cache.ContainsKey(key))
+            return null;
+
+        UpdateFrequency(key);
+        
+        return _cache[key].Value.CacheValue;
+    }
+    
+    public void Put(string key, string value)
+    {
+        if (_cache.ContainsKey(key))
+        {
+            _cache[key].Value.CacheValue = value;
+            UpdateFrequency(key);
+            return;
+        }
+
+        if (_cache.Count == _length)
+        {
+            _cache.Remove(_frequencyList[_minFrequency].Last.Value.CacheKey);
+            _frequencyList[_minFrequency].RemoveLast();
+        }
+
+        var node = new LinkedListNode<CacheItem>(
+            new CacheItem
+            {
+                CacheKey = key,
+                CacheValue = value,
+                Frequency = 1
+            });
+        
+        _cache.Add(key, node);
+        
+        if(!_frequencyList.ContainsKey(1))
+            _frequencyList.Add(1, new LinkedList<CacheItem>());
+
+        _frequencyList[1].AddLast(node);
+        _minFrequency = 1;
+    }
+
+    private void UpdateFrequency(string key)
+    {
+        var node = _cache[key];
+
+        if (_minFrequency == node.Value.Frequency)
+            _minFrequency++;
+        
+        _frequencyList[node.Value.Frequency].Remove(node);
+        
+        if(!_frequencyList.ContainsKey(node.Value.Frequency+1))
+            _frequencyList.Add(node.Value.Frequency+1, new LinkedList<CacheItem>());
+        
+        _frequencyList[node.Value.Frequency+1].AddFirst(node);
+        node.Value.Frequency++;
+    }
+}
